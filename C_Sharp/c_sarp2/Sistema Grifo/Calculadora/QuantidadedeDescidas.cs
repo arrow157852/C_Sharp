e806905fd5bfbb7dec0a880beb73982b79e95114
gabriel <bielh2001@gmail.com>
2024-03-13 14:06:13 -0300

    atualizacao logica

---
+++ C_Sharp/c_sarp2/Sistema Grifo/Calculadora/QuantidadedeDescidas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Grifo.Calculadora
+{
+    internal class QuantidadedeDescidas
+    {
+        public struct ValoresQuantidade
+        {
+            public int Descidas;
+            public int AnelDeEquilibrio;
+        }
+        public ValoresQuantidade quantDescidas()
+        {
+            UserControl2 u = new UserControl2();
+            int descidas = 0;
+            int anelDeEquilibrio= 0;
+            string publicCbnpValue = u.cbnp.Text;
+            float perimetroSpda = (float)u.nudperimetro.Value;
+            float alturaSpda = (float)u.nudaltura.Value;
+
+            if (publicCbnpValue == "1" || publicCbnpValue == "2")
+            {
+                descidas = (int)perimetroSpda / 12;
+                anelDeEquilibrio = (int)(alturaSpda / 10 - 1);
+            }
+            else if (publicCbnpValue == "3")
+            {
+                descidas = (int)perimetroSpda / 18;
+                anelDeEquilibrio = (int)(alturaSpda / 15 - 1);
+            }
+            else if (publicCbnpValue == "4")
+            {
+
+                descidas = (int)perimetroSpda / 24;
+                anelDeEquilibrio = (int)(alturaSpda / 20 - 1);
+            }
+            return new ValoresQuantidade { Descidas = descidas, AnelDeEquilibrio = anelDeEquilibrio };
+        }
+       
+    }
+}
+
